b75c02ecdd9cb0ae84493c3f88e063a96a8be482
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-03-20 18:27:28 -0400

    Update list_node.h

--- list_node.h
+++ list_node.h
@@ -1,7 +1,10 @@
- #ifndef 
+#ifndef LIST_NODE_H
+#define LIST_NODE_H
  
- struct ListNode {
+struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
- };
+};
+
+#endif
