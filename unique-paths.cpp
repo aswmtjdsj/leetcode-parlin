faeccc0d25cf39695ddee49482a9c45cc113b07b
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-04-06 16:37:16 -0400

    Create unique-paths.cpp

---
+++ unique-paths.cpp
@@ -0,0 +1,30 @@
+#include "header.h"
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        int dp[105][105];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if( i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                }
+                else if(i == 1) {
+                    dp[i][j] = dp[i][j-1];
+                }
+                else if(j == 1) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main() {
+}
