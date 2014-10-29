5a97119c038d2acd82ca56914149d0cb87f8c7c5
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-10-29 16:38:41 -0400

    valid-sudoku modified to a more reasonable ver

--- valid-sudoku.cpp
+++ valid-sudoku.cpp
@@ -3,46 +3,45 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
-        int n = board.size();
-        //horizontal
-        for(int i = 0; i < n; i++) {
-            int vis[10] = {0};
-            for(int j = 0; j < n; j++) {
-                if(isdigit(board[i][j])) {
-                    vis[board[i][j]-'0']++;
-                    if(vis[board[i][j]-'0'] > 1) {
+        for(int i = 0; i < 9; i++) {
+            set <char> S;
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    auto ret = S.insert(board[i][j]);
+                    if(ret.second == false) {
                         return false;
                     }
                 }
             }
         }
-        //vertical
-        for(int i = 0; i < n; i++) {
-            int vis[10] = {0};
-            for(int j = 0; j < n; j++) {
-                if(isdigit(board[j][i])) {
-                    vis[board[j][i]-'0']++;
-                    if(vis[board[j][i]-'0'] > 1) {
+        
+        for(int j = 0; j < 9; j++) {
+            set <char> S;
+            for(int i = 0; i < 9; i++) {
+                if(board[i][j] != '.') {
+                    auto ret = S.insert(board[i][j]);
+                    if(ret.second == false) {
                         return false;
                     }
                 }
             }
         }
-        //inner 9
-        for(int i = 0; i < n; i++) {
-            int vis[10] = {0};
-            for(int j = 0; j < n; j++) {
-                int row = i / 3, col = i % 3;
-                int subrow = j / 3, subcol = j % 3;
-                int ai = row * 3 + subrow, aj = col * 3 + subcol;
-                if(isdigit(board[ai][aj])) {
-                    vis[board[ai][aj] - '0']++;
-                    if(vis[board[ai][aj] - '0'] > 1) {
-                        return false;
+        
+        for(int k = 0; k < 9; k++) {
+            int y = (k / 3) * 3, x = (k % 3) * 3;
+            set <char> S;
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if(board[i+y][j+x] != '.') {
+                        auto ret = S.insert(board[i+y][j+x]);
+                        if(ret.second == false) {
+                            return false;
+                        }
                     }
                 }
             }
         }
+        
         return true;
     }
 };
